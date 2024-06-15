22dadb81e601905941e41cd33d63de3761977d51
Chris Lokhorst <chris.lokhorst@hva.nl>
2024-06-15 22:57:05 +0200

    __m128, no difference, but idk, worth a shot i guess

--- src/matrix.h
+++ src/matrix.h
@@ -9,7 +9,7 @@ void mat_rotate(float *matrix, float x, float y, float z, float angle);
 void mat_vec_multiply(float *vector, float *a, float *b);
 void mat_multiply(float *matrix, float *a, float *b);
 void mat_apply(float *data, float *matrix, int count, int offset, int stride);
-void frustum_planes(__m256 planes[6][4], int radius, float *matrix);
+void frustum_planes(__m128 planes[6][4], int radius, float *matrix);
 void mat_frustum(
     float *matrix, float left, float right, float bottom,
     float top, float znear, float zfar);
